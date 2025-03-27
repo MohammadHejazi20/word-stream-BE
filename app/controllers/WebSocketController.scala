ad492a4501505106d37c4e665d8350d1f696d8e0
Mohammad Hejazi <89161570+MohammadHejazi20@users.noreply.github.com>
2025-03-27 10:15:22 +0100

    Refactor WebSocket integration: bind WebSocketHubActor, enhance WebSocketActor for update checks, and streamline service dependencies

--- app/controllers/WebSocketController.scala
+++ app/controllers/WebSocketController.scala
@@ -1,30 +1,23 @@
 package controllers
 
-import actors.{WebSocketActor, WebSocketHubActor}
+import actors.{WebSocketActor}
 import services.{BlogFetcherService, WordCountService}
 import play.api.mvc._
 import play.api.libs.streams.ActorFlow
 import play.api.libs.json.JsValue
-import org.apache.pekko.actor.{ActorRef, ActorSystem, Props}
+import org.apache.pekko.actor.{ActorRef, ActorSystem}
 import org.apache.pekko.stream.Materializer
-import javax.inject.{Inject, Singleton}
+import javax.inject.{Inject, Named, Singleton}
 import scala.concurrent.ExecutionContext
 
 @Singleton
 class WebSocketController @Inject() (
     cc: ControllerComponents,
-    ws: play.api.libs.ws.WSClient,
-    config: play.api.Configuration
+    blogFetcher: BlogFetcherService,
+    wordCounter: WordCountService,
+    @Named("webSocketHub") hub: ActorRef // Is automatically injected by Guice
 )(implicit system: ActorSystem, mat: Materializer, ec: ExecutionContext)
     extends AbstractController(cc) {
-
-  // === Create central hub actor
-  private val hub: ActorRef =
-    system.actorOf(Props[WebSocketHubActor], "webSocketHub")
-
-  // === Services used by each socket connection
-  private val blogFetcher = new BlogFetcherService(ws, config)
-  private val wordCounter = new WordCountService(hub)
 
   def websocket: WebSocket = WebSocket.accept[JsValue, JsValue] { _ =>
     ActorFlow.actorRef { clientRef =>
--- app/controllers/WordCountController.scala
+++ app/controllers/WordCountController.scala
@@ -3,8 +3,6 @@ package controllers
 import play.api.mvc._
 import play.api.libs.json._
 import services.{WordCountService, BlogFetcherService}
-import org.apache.pekko.actor.ActorSystem
-import org.apache.pekko.stream.Materializer
 import javax.inject._
 import scala.concurrent.ExecutionContext
 
@@ -22,15 +20,9 @@ import scala.concurrent.ExecutionContext
 class WordCountController @Inject() (
     cc: ControllerComponents,
     blogFetcher: BlogFetcherService,
-    webSocketController: WebSocketController // Inject the WebSocketController
-)(implicit system: ActorSystem, ec: ExecutionContext, mat: Materializer)
+    wordCountService: WordCountService
+)(implicit ec: ExecutionContext)
     extends AbstractController(cc) {
-
-  // === Shared actor hub from WebSocketController
-  private val hub = webSocketController.getHub
-
-  // === Instantiate the WordCountService with the shared hub (sends to hub)
-  private val wordCountService = new WordCountService(hub)
 
   def fetchAndProcessBlogs: Action[AnyContent] = Action.async {
     blogFetcher.fetchPosts().flatMap { jsonResponse =>
